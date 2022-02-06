64efcfc902f2e10bb44c1e2c83abf26821de7321
lisandrolan <lisandrolan@gmail.comsss>
2022-02-06 10:56:50 +0200

    feat: new story

--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
             prerelease: true,
         },
         {
-            name: "feature-*",
+            name: "feature*",
             channel: "development",
             prerelease: true
         }
